80e2c3e60f547b161900d9492907fafd097e2028
moritz wolter <wolter@cs.uni-bonn.de>
2019-06-17 17:25:45 +0200

    mnist update.

--- mnist_numpy.py
+++ mnist_numpy.py
@@ -1,2 +1,10 @@
 import numpy as np
 
+import struct
+
+
+with open('data/t10k-images-idx3-ubyte', 'rb') as f:
+    magic, size = struct.unpack(">II", f.read(8))
+    nrows, ncols = struct.unpack(">II", f.read(8))
+    data = np.fromfile(f, dtype=np.dtype(np.uint8).newbyteorder('>'))
+    data = data.reshape((size, nrows, ncols))
