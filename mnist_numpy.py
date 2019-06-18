58406b8a3d934475bfa655b08af640336e805832
moritz <wolter@cs.uni-bonn.de>
2019-06-18 17:29:37 +0200

    mnist grad update.

--- mnist_numpy.py
+++ mnist_numpy.py
@@ -1,3 +1,7 @@
+# a numpy only le net implementation
+# based on http://yann.lecun.com/exdb/publis/pdf/lecun-98.pdf
+
+import ipdb
 import struct
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,5 +21,54 @@ print(lbl_data_train[idx])
 plt.imshow(img_data_train[idx, :, :])
 plt.show()
 
-# Set up a lenet
 
+def normalize(data):
+    mean = np.mean(data)
+    var = np.mean(data)
+    return (data-mean)/var
+
+
+img_data_train_norm = normalize(img_data_train)
+
+print(lbl_data_train[idx])
+plt.imshow(img_data_train_norm[idx, :, :])
+plt.show()
+
+
+def im2col(img, kernel_shape, stride, padding='VALID'):
+    assert padding == 'VALID'
+    img_shape = img.shape
+    patches = []
+    for row_pos in range(0, img_shape[0]-kernel_shape[0]+1, stride):
+        for col_pos in range(0, img_shape[1]-kernel_shape[1]+1, stride):
+            row_strt = row_pos
+            row_stop = int(row_pos + kernel_shape[0])
+            col_strt = col_pos
+            col_stop = int(col_pos + kernel_shape[1])
+            img_patch = img[row_strt:row_stop, col_strt:col_stop]
+            print(row_strt, row_stop, col_strt, col_stop)
+            patches.append(img_patch.flatten())
+    return np.stack(patches)
+
+
+def col2im(col_res, img_shape, kernel_shape, stride, padding='VALID'):
+    assert padding == 'VALID'
+    rows = img_shape[0] 
+    cols = None
+    return None
+
+
+
+def convolution(kernel, img):
+    # im2col
+    # matmul
+
+    pass
+
+
+test_patches = im2col(img_data_train[0, :, :], kernel_shape=(3, 3),
+                      stride=1)
+kernel = np.random.randn(3, 3)
+kernel = kernel.flatten()
+kernel = np.expand_dims(kernel, -1)
+mul_conv = np.matmul(test_patches, kernel)
